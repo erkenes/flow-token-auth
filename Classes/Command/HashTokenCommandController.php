ee3fda94a9db5701563ddb631b1646f6d2e397fb
Karsten Dambekalns <karsten@dambekalns.de>
2026-08-11 16:12:58 +0200

    Make label and expiresAt parameters nullable
    Updated parameters in createHashTokenCommand to allow nullable types for label and expiresAt.
    
    Also add doc comments for the Flow help output.

--- Classes/Command/HashTokenCommandController.php
+++ Classes/Command/HashTokenCommandController.php
@@ -29,12 +29,14 @@ class HashTokenCommandController extends CommandController
      * Create a hash token to login with, authenticating the given roles.
      *
      * @param array $roleNames
+     * @param ?string $label An optional label for the token
+     * @param ?string $expiresAt An optional expiry date / time for the token (anything DateTime($expiresAt) can parse)
      * @throws \Neos\Flow\Persistence\Exception\IllegalObjectTypeException
      */
     public function createHashTokenCommand(
         array $roleNames,
-        string $label = null,
-        string $expiresAt = null
+        ?string $label = null,
+        ?string $expiresAt = null
     ): void
     {
         if (!empty($expiresAt)) {
