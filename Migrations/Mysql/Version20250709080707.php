5e0877c68017b2d38c8af6e214a7423d080fe034
Karsten Dambekalns <karsten@dambekalns.de>
2026-08-11 16:00:16 +0200

    Adjust DB migration

--- Migrations/Mysql/Version20250709080707.php
+++ Migrations/Mysql/Version20250709080707.php
@@ -7,22 +7,18 @@ namespace Neos\Flow\Persistence\Doctrine\Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20250709080707 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add label, expiresat and createdat';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MariaDb1027Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MariaDb1027Platform'."
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQLPlatform'."
         );
 
         $this->addSql('ALTER TABLE flownative_tokenauthentication_security_model_hashandroles ADD label VARCHAR(255) DEFAULT NULL, ADD expiresat DATETIME DEFAULT NULL, ADD createdat DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
@@ -30,10 +26,9 @@ final class Version20250709080707 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf(
-            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MariaDb1027Platform,
-            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MariaDb1027Platform'."
+            !$this->connection->getDatabasePlatform() instanceof \Doctrine\DBAL\Platforms\MySQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQLPlatform'."
         );
 
         $this->addSql('ALTER TABLE flownative_tokenauthentication_security_model_hashandroles DROP label, DROP expiresat, DROP createdat');
